cf93bdb337bb2067c84f96a910e9c5e7850d7e73
nazonohito51 <nazonohito51@hotmail.com>
2015-07-20 22:43:25 +0900

    fix my_script

--- my_script.php
+++ my_script.php
@@ -4,10 +4,12 @@ $change_files = null;
 $ret = exec('git diff --name-only master...HEAD', $change_files);
 
 foreach($change_files as $file) {
-    $path = realpath($file);
-    $cmd = "php .\PHP_CodeSniffer\scripts\phpcs ${path}";
-    $ret = exec($cmd, $output);
+    //$path = realpath($file);
+    $path = $file;
+    $cmd = "php .\PHP_CodeSniffer\scripts\phpcs --standard=PSR2 --encoding=utf-8 --extensions=php ${path}";
+    $ret = exec($cmd, $output, $ret);
     var_dump($output);
+    //var_dump($ret);
     $output = null;
 }
 
