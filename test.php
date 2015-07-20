907b1ecb90e6ff11d0fef4cc9d9455ac87824099
nazonohito51 <nazonohito51@hotmail.com>
2015-07-20 17:20:41 +0900

    test2

---
+++ test.php
@@ -0,0 +1,44 @@
+<?php
+/*
+class TestClass
+{
+    var $property;
+    
+    function TestClass($property) {
+        $this->property = $property;
+    }
+}
+
+$a = new TestClass(1);
+echo $a . "\n";
+//echo & $a . "\n";
+var_dump(get_class($a));
+var_dump(is_a($a, TestClass));
+var_dump(TestClass);
+*/
+
+
+/*
+$str = 'string';
+var_dump(is_string($str));
+var_dump(is_int($str));
+var_dump(is_bool($str));
+var_dump(is_object($str));
+var_dump(is_array($str));
+
+$null_var = null;
+var_dump(is_string($null_var));
+var_dump(is_int($null_var));
+var_dump(is_bool($null_var));
+var_dump(is_object($null_var));
+var_dump(is_array($null_var));
+
+$arr = array();
+var_dump(is_string($arr));
+var_dump(is_int($arr));
+var_dump(is_bool($arr));
+var_dump(is_object($arr));
+var_dump(is_array($arr));
+*/
+
+?>
