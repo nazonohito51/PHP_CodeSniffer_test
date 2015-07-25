0ef7b4f1f277ddb7671514e6e6616b68e7c9e17f
nazonohito51 <nazonohito51@hotmail.com>
2015-07-25 18:19:25 +0900

    fix my_script

--- my_script.php
+++ my_script.php
@@ -1,14 +1,21 @@
 <?php
+$extensions = array('php');
 
 $change_files = null;
 $ret = exec('git diff --name-only master...HEAD', $change_files);
 
-foreach($change_files as $file) {
+foreach ($change_files as $file) {
     //$path = realpath($file);
     $path = $file;
-    $cmd = "php .\PHP_CodeSniffer\scripts\phpcs --standard=PSR2 --encoding=utf-8 --extensions=php ${path}";
-    $ret = exec($cmd, $output, $ret);
-    var_dump($output);
+    
+    $pathinfo = pathinfo($file);
+    if (!in_array($pathinfo['extension'], $extensions)) {
+        continue;
+    }
+    
+    $cmd = "php .\PHP_CodeSniffer\scripts\phpcbf --standard=PSR2 --encoding=utf-8 --extensions=php ${path}";
+    $ret = passthru($cmd, $ret);
+    //var_dump($output);
     //var_dump($ret);
     $output = null;
 }
