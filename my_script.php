55b58e59dcd5ebb82cc061c21592d30cd0cb6de6
nazonohito51 <nazonohito51@hotmail.com>
2015-07-26 04:38:35 +0900

    fix my_script

--- my_script.php
+++ my_script.php
@@ -1,23 +1,35 @@
 <?php
+$exit_val = 0;
+$script_path = '.\PHP_CodeSniffer\scripts';
+$ruleset_path = '.\config/PHP_CodeSniffer/ruleset.xml';
+$encoding = 'utf-8';
+
+$script = 'phpcs';
+if ($argv[1] == 1) {
+    $script = 'phpcbf';
+}
+
+// 処理対象のextension指定
 $extensions = array('php');
 
+// masterブランチから変更されたファイルのみを抽出する
 $change_files = null;
 $ret = exec('git diff --name-only master...HEAD', $change_files);
 
 foreach ($change_files as $file) {
-    //$path = realpath($file);
     $path = $file;
-    
-    $pathinfo = pathinfo($file);
-    if (!in_array($pathinfo['extension'], $extensions)) {
+    $path_info = pathinfo($file);
+
+    // 処理対象のextensionでなければ無視する
+    if (!in_array($path_info['extension'], $extensions)) {
         continue;
     }
-    
-    $cmd = "php .\PHP_CodeSniffer\scripts\phpcs --standard=config/PHP_CodeSniffer/ruleset.xml --encoding=utf-8 --extensions=php ${path}";
-    $ret = passthru($cmd, $ret);
-    //var_dump($output);
-    //var_dump($ret);
-    $output = null;
+
+    // phpcsの実行
+    $cmd = "php ${script_path}\\${script} --standard=${ruleset_path} --encoding=${encoding} ${path}";
+    passthru($cmd, $ret);
+
+    $exit_val |= $ret;
 }
 
-?>
+return($exit_val);
